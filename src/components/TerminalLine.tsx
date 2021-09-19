52de0195c0965f2fdaffd4b0d1e24da3333b6756
mugayoshi <muga.y.j3039@gmail.com>
2021-09-19 12:26:07 +0900

    [feat]: add display animation on terminal line component

--- src/components/TerminalLine.tsx
+++ src/components/TerminalLine.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
 import classNames from "classnames";
+import { useState, VFC, useEffect } from "react";
 
 interface TerminalLineProps {
     classes: string[];
@@ -9,18 +9,33 @@ interface TerminalLineProps {
     totalQuestions: number;
 }
 
-export const TerminalLine: React.FC<TerminalLineProps> = ({
+export const TerminalLine: VFC<TerminalLineProps> = ({
     classes,
     questionIndex,
     question,
     questionAnswer,
     totalQuestions,
 }) => {
+    const [displayedLine, setDisplayedLine] = useState("");
+    const [displayedCharIndex, setCharIndex] = useState(0);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (displayedCharIndex > questionAnswer.length) {
+                return;
+            }
+            const currentIndex = displayedCharIndex;
+            const charts = Array.from(questionAnswer);
+            setDisplayedLine(charts.slice(0, displayedCharIndex).join(""));
+            setCharIndex(currentIndex + 1);
+        }, 100);
+        return () => clearInterval(timer);
+    }, [displayedCharIndex, questionAnswer]);
+
     return (
         <>
             <p className={classNames(...classes)}>
-                ✅ [{questionIndex}/{totalQuestions}] {question}:{" "}
-                {questionAnswer}
+                {displayedCharIndex > questionAnswer.length ? "✅ " : " "}[
+                {questionIndex}/{totalQuestions}] {question}: {displayedLine}
             </p>
         </>
     );
